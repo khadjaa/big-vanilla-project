eb29e6da7ef183307dde2f6faae3dfddb587d48e
khadjaa <islomkhadja2001@icloud.com>
2023-02-21 17:23:11 +0300

    created array devs

---
+++ src/05.map/05.ts
@@ -0,0 +1,28 @@
+export type ManType = {
+    name: string
+    age: number
+}
+
+const people: ManType[] = [
+    {name: "Andrew Ivanov", age: 33},
+    {name: "islam isa", age: 21},
+    {name: "Dmitry San", age: 27}
+]
+
+const dimychTransformator = (man: ManType) => {
+}
+
+const devs = [
+    {
+        stack: ["css, html", "js, ts", "react"],
+        firstName: "Andrew", lastName: "Ivanov"
+    },
+    {
+        stack: ["css, html", "js, ts", "react"],
+        firstName: "islam", lastName: "isa"
+    },
+    {
+        stack: ["css, html", "js, ts", "react"],
+        firstName: "Dmitry", lastName: "San"
+    },
+]
